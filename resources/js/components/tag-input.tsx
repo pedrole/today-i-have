9a95079989a181f3b8f45e98fb4ee0e248b0cce7
Pedro Leal <lealpedro2009@gmail.com>
2026-07-13 19:47:45 +0100

    chore: save local in-progress changes

--- resources/js/components/tag-input.tsx
+++ resources/js/components/tag-input.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import { X } from 'lucide-react';
+import * as React from 'react';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
